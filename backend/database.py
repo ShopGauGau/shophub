c5dfbca91eee7ac9830a46391005db6d2abcf2a2
lethanhthien <lethanhthien04@gmail.com>
2026-07-10 18:02:54 +0700

    Update database link cloud

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
-# database.py
 from sqlalchemy import create_engine
 
-DATABASE_URL = "mssql+pyodbc:///?odbc_connect=DRIVER={ODBC Driver 17 for SQL Server};SERVER=.;DATABASE=RoomHubDB;Trusted_Connection=yes;TrustServerCertificate=yes;"
+# Chuỗi kết nối đã được gắn mật khẩu vapcom123 của ní vô rồi nha
+DATABASE_URL = "mssql+pyodbc://db_acbd86_roomhub_admin:vapcom123@sql8020.site4now.net/db_acbd86_roomhub?driver=ODBC+Driver+17+for+SQL+Server"
+
 engine = create_engine(DATABASE_URL)
