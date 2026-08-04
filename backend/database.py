d0e0a92c1f3fc644ea9024cd98601b6bf832afc0
lethanhthien <lethanhthien04@gmail.com>
2026-08-04 20:47:18 +0700

    database

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 
-# Chuỗi kết nối đã được gắn mật khẩu vapcom123 của ní vô rồi nha
-DATABASE_URL = "mssql+pyodbc://db_acbd86_roomhub_admin:vapcom123@sql8020.site4now.net/db_acbd86_roomhub?driver=ODBC+Driver+17+for+SQL+Server"
-
+# Đã đổi thành pymssql siêu mượt
+DATABASE_URL = "mssql+pymssql://db_acbd86_roomhub_admin:vapcom123@sql8020.site4now.net/db_acbd86_roomhub"
 engine = create_engine(DATABASE_URL)
